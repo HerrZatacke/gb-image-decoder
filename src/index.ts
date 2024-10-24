cc53c5fb90146f785044211898c12b72f1cc2d55
Andreas Hahn <zatacke@gmail.com>
2024-10-24 12:28:55 +0200

    add additional export

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ export { decodeTile } from './functions/decodeTile';
 export { getRGBValue } from './functions/getRGBValue';
 export { tileIndexIsPartOfFrame } from './functions/tileIndexIsPartOfFrame';
 export { maxTiles } from './functions/maxTiles';
-export { ExportFrameMode } from './constants/enums';
+export { ChannelKey, ExportFrameMode } from './constants/enums';
 export { BW_PALETTE, BW_PALETTE_HEX, SKIP_LINE } from './constants/base';
 export { BlendMode, blendModeNewName } from './constants/blendModes';
 
